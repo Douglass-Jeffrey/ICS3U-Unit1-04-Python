a5b6ae9dcecfbfef70a74a6ca29720ea33dedbef
Ubuntu <ubuntu@ip-172-31-19-198.ec2.internal>
2019-09-13 18:33:52 +0000

    initial commit

---
+++ rectangle_calc.py
@@ -0,0 +1,18 @@
+# 1/usr/bin/env python3
+
+# Created by: Douglass Jeffrey
+# Created on: September 2019
+# This is a program that calculates the area and perimeter of a rectange
+#       with dimensions 5cm x 3cm
+
+
+def main():
+    print("If a rectangle has the dimensions")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm^2".format(3*5))
+    print("Perimeter is {}cm".format(2*(3+5)))
+
+
+if __name__ == "__main__":
+    main()
